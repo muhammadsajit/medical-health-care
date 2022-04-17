6713ec1fc17667af4ec8b4591576d8a087284ed5
muhammadsajit <sajit.du.niter@gmail.com>
2022-04-17 23:41:37 +0600

    service added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Service.css'
 
 const Service = ({service}) => {
     const{id,name,img,description,price}=service;
@@ -8,12 +9,12 @@ const Service = ({service}) => {
        navigate(`/service/${id}`)  
     }
     return (
-        <div className='service'>
-            <img className='w-100' src={img} alt="" />
+        <div className='service '>
+            <img style={{height:'300px'}} className='w-100' src={img} alt="" />
             <h2>{name}</h2>
             <p>Price:{price}</p>
             <p><small>{description}</small></p>
-            <button onClick={()=>navigateCheckout(id)} className='btn btn-primary'>Detail</button>
+            <button  onClick={()=>navigateCheckout(id)} className='btn btn-primary  '>Detail</button>
         </div>
     );
 
