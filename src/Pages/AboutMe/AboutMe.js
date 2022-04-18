576538b0959b82877566b01b55b4871e037f67bc
muhammadsajit <sajit.du.niter@gmail.com>
2022-04-18 16:46:00 +0600

    about me updated

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
     return (
         <div>
             <h1 className='text-center text-primary'>About me</h1>
-            <div className=''>
+            <div className='profile'>
                 <img style={{height:"400px"}} className='w-25 mx-auto d-block' src={profile} alt="" />
                 <h1 className='text-center text-primary'>Name:Muhammad Sajit</h1>
                 <p className='ps-5'><span className="goal">My goal</span>:As a Web Developer,the main objective when working as a developer is to be productive and deliver value to the company.As a developer or a CTO,the aim is to build practical application that will be 100% successfull.To achieve this specific goals need to be set,Which include:Enhanching networking Skills</p>
